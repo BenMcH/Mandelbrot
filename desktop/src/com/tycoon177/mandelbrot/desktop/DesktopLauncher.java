130bc6762c846728146fc8e17e9ee9d05e208637
= <benjamin.mchone@gmail.com>
2015-01-30 00:05:59 -0600

    Committed for an empty Libgdx project

---
+++ desktop/src/com/tycoon177/mandelbrot/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tycoon177.mandelbrot.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tycoon177.mandelbrot.Mandelbrot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mandelbrot(), config);
+	}
+}
